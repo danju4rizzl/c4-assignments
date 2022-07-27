e903831297dc827b807fe57aa6155815cea5b1f0
McOtech <otienopaul36@gmail.com>
2022-07-27 15:59:54 +0300

    assignment 1

---
+++ assignment_1/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    run(0, 10);
+}
+
+/**
+ * This function prints in the terminal whether an integer is an even
+ *  or odd number given a range of number.
+ * 
+ * It accepts two parameters:
+ * start: start value of range
+ * limit: end value of range
+ * 
+ * Example:
+ * 
+ * run(0, 10);
+ * 
+ * Should print:
+ * 0 is an even number
+ * 1 is an odd number
+ * 2 is an even number
+ * 3 is an odd number
+ *          .
+ *          .
+ *          .
+ * 9 is an odd number
+ */
+pub fn run(start: u32, limit: u32) {
+    // Write your code here
+    todo!();
+}
